6a1f77a81c436571463e93e62def5ad5cf252038
Glitch (hello-express) <none>
2021-03-06 01:30:04 +0000

    🕹🚟 Checkpoint ./public/js/main.js:29404343/5026

--- public/js/main.js
+++ public/js/main.js
@@ -4,8 +4,15 @@ var scene = new THREE.Scene();
 var viewSize = 50;
 var aspectRatio = window.innerWidth / window.innerHeight;
 //var camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 1000 );
-var camera = new THREE.OrthographicCamera( -aspectRatio * viewSize / 2, aspectRatio * viewSize /2, viewSize / 2, -viewSize /2, -1000, 1000)
-scene.add( camera );
+var camera = new THREE.OrthographicCamera(
+  (-aspectRatio * viewSize) / 2,
+  (aspectRatio * viewSize) / 2,
+  viewSize / 2,
+  -viewSize / 2,
+  -1000,
+  1000
+);
+scene.add(camera);
 
 camera.position.x = 10;
 camera.position.y = 5;
@@ -15,50 +22,47 @@ camera.lookAt(scene.position);
 var light = new THREE.AmbientLight(0xffffff, 0.75);
 scene.add(light);
 
-var directionalLight = new THREE.DirectionalLight( 0xffffff, 0.5 );
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
 directionalLight.position = (1, 1, 1);
-scene.add( directionalLight );
+scene.add(directionalLight);
 
 var renderer = new THREE.WebGLRenderer();
 // var renderer = new THREE.SVGRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-renderer.setClearColor( 0xd1e3ff, 1 );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.setClearColor(0xd1e3ff, 1);
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
-var axesHelper = new THREE.AxesHelper( 250 );
-scene.add( axesHelper );
+var axesHelper = new THREE.AxesHelper(250);
+scene.add(axesHelper);
 
-///////////////////////////////////////////////// MATERIALS 
+///////////////////////////////////////////////// MATERIALS
 
-var clear = new THREE.MeshLambertMaterial({ 
+var clear = new THREE.MeshLambertMaterial({
   color: 0x000000,
   transparent: true,
   opacity: 0.25,
-  depthWrite: false,
+  depthWrite: false
 });
 
-var clear2 = new THREE.MeshLambertMaterial({ 
+var clear2 = new THREE.MeshLambertMaterial({
   color: 0x000000,
   transparent: true,
   opacity: 0.75,
-  depthWrite: false,
+  depthWrite: false
 });
 
+var line_mat = new THREE.LineBasicMaterial({
+  color: 0xff0000,
+  linewidth: 1
+});
 
-var line_mat = new THREE.LineBasicMaterial( {
-	color: 0xff0000,
-	linewidth: 1,
-} );
-
-var color = new THREE.MeshLambertMaterial( {
-   color: 0xff8000, 
-   wireframe: false 
-} );
+var color = new THREE.MeshLambertMaterial({
+  color: 0xff8000,
+  wireframe: false
+});
 
 ///////////////////////////////////////////////// DATA STRUCTURES
-
-
 
 ///////////////////////////////////////////////// GUI CONTROLS
 
@@ -71,21 +75,20 @@ var color = new THREE.MeshLambertMaterial( {
 // var changeSpeed = gui.add(guiControls, 'speed', 1, 10).step(1);
 // var toggleAssembly = gui.add(guiControls, 'showAssembly');
 
-
 ///////////////////////////////////////////////// ANIMATE LOOP
 
 function animate() {
-  requestAnimationFrame( animate );
-  
-//   renderer.setClearColor( guiControls.background, 1 );
-//   clear.color.setHex( guiControls.assembly );
-//   clear2.color.setHex( guiControls.assembly );
-//   line_mat.color.setHex( guiControls.stroke );
-//   color.color.setHex( guiControls.stroke );
+  requestAnimationFrame(animate);
 
-//   updateAssembly( guiControls.play );
-//   if (index != -1) path.geometry.attributes.position.needsUpdate = true; 
+  //   renderer.setClearColor( guiControls.background, 1 );
+  //   clear.color.setHex( guiControls.assembly );
+  //   clear2.color.setHex( guiControls.assembly );
+  //   line_mat.color.setHex( guiControls.stroke );
+  //   color.color.setHex( guiControls.stroke );
 
-  renderer.render( scene, camera );
+  //   updateAssembly( guiControls.play );
+  //   if (index != -1) path.geometry.attributes.position.needsUpdate = true;
+
+  renderer.render(scene, camera);
 }
 animate();
