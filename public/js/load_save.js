9de83bdfcba02663166003526319d6dc6df3e171
Glitch (hello-express) <none>
2021-03-07 08:44:29 +0000

    🐥📼 Checkpoint ./public/style_mobileview.css:29407381/47:29424838/55 ./public/style.css:29404343/185 ./public/js/load_save.js:29404343/2758 ./public/index.html:21864987/4:29404343/139

--- public/js/load_save.js
+++ public/js/load_save.js
@@ -1,22 +1,26 @@
 function save() {
   var filename = prompt("Enter filename");
-  $.get("db/save?filename=" + filename, function(data) {
-    alert("Success!");
-  });
+  if (filename != null) {
+    $.get("db/save?filename=" + filename, function(data) {
+      alert("Success!");
+    });
+  }
 }
 
 function load() {
   var filename = prompt("Enter filename");
-  $.get("db/load?filename=" + filename, function(packet) {
-    for (let other_client_id in packet) {
-      if (clientData[other_client_id] == undefined)
-        clientData[other_client_id] = {};
-      clientData[other_client_id] = Object.assign(
-        {},
-        clientData[other_client_id],
-        packet[other_client_id]
-      );
-      updateAllLines(packet[other_client_id]);
-    }
-  });
+  if (filename != null) {
+    $.get("db/load?filename=" + filename, function(packet) {
+      for (let other_client_id in packet) {
+        if (clientData[other_client_id] == undefined)
+          clientData[other_client_id] = {};
+        clientData[other_client_id] = Object.assign(
+          {},
+          clientData[other_client_id],
+          packet[other_client_id]
+        );
+        updateAllLines(packet[other_client_id]);
+      }
+    });
+  }
 }
