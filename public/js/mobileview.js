b4b483671e7dd202f54f15d83871ea4b672bac5d
Glitch (hello-express) <none>
2021-03-07 00:27:27 +0000

    🛬🎣 Checkpoint ./public/mobileview.html:29404343/186:29407381/121 ./public/js/websocket.js:29404343/133 ./public/style_mobileview.css:29404343/1299 ./public/js/mobileview.js:29404343/195

--- public/js/mobileview.js
+++ public/js/mobileview.js
@@ -2,10 +2,11 @@
 // tracker icon changing
 function change_picture() {
   var tracker = document.getElementById("tracker");
+  var status = document.getElementById("status");
   console.log(name);
   var name = tracker.style.backgroundImage;
   tracker.style.backgroundImage = name.endsWith("assets/aruco-220.svg\")") ? "url(\"assets/aruco-1023.svg\")" : "url(\"assets/aruco-220.svg\")";
-  tracker.innerHTML = name.endsWith("assets/aruco-220.svg\")") ? "OFF" : "ON";
+  status.innerHTML = name.endsWith("assets/aruco-220.svg\")") ? "OFF" : "ON";
   openFullscreen();
 }
 
--- public/js/websocket.js
+++ public/js/websocket.js
@@ -1,9 +1,6 @@
 
 const socket = io();
 
-function sendUpdate(lines) {
-  socket.emit('client-update', lines); // Call this line whenever you want to send data to the server
-}
 
 socket.on("server-update", lines => {
   console.log(lines);
